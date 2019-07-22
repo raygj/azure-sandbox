57d58f00f954c01aaf9accbc3ed96240535f2631
Jim Ray <ray.jim@gmail.com>
2019-07-22 09:56:29 -0400

    test run of basic AZ resources

--- main.tf
+++ main.tf
@@ -1,65 +1,36 @@
-variable "username" {
-  default = "myusername"
+# https://github.com/kawsark/azure-terraform-workshop/tree/master/challenges/03-azurevm
+
+provider "azurerm" {
+  version = "= 1.4"
+}
+
+terraform {
+  required_version = ">=0.11.7"
+}
+
+variable "name" {
+  default = "jray"
+}
+
+variable "location" {
+  default = "eastus"
 }
 
 resource "azurerm_resource_group" "main" {
-  name     = "aci-helloworld"
-  location = "eastus"
+  name     = "${var.name}-rg"
+  location = "${var.location}"
 }
 
-resource "azurerm_storage_account" "main" {
-  name                     = "azcntinststor${var.username}"
-  resource_group_name      = "${azurerm_resource_group.main.name}"
-  location                 = "${azurerm_resource_group.main.location}"
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
-
-resource "azurerm_storage_share" "main" {
-  name                 = "aci-test-share"
-  resource_group_name  = "${azurerm_resource_group.main.name}"
-  storage_account_name = "${azurerm_storage_account.main.name}"
-  quota                = 1
-}
-
-resource "azurerm_container_group" "main" {
-  name                = "aci-helloworld"
+resource "azurerm_virtual_network" "main" {
+  name                = "${var.name}-vnet"
+  address_space       = ["10.0.0.0/16"]
   location            = "${azurerm_resource_group.main.location}"
   resource_group_name = "${azurerm_resource_group.main.name}"
-  ip_address_type     = "public"
-  dns_name_label      = "aci-${var.username}"
-  os_type             = "linux"
+}
 
-  container {
-    name   = "helloworld"
-    image  = "microsoft/aci-helloworld"
-    cpu    = "0.5"
-    memory = "1.5"
-    port   = "80"
-
-    environment_variables {
-      "NODE_ENV" = "testing"
-    }
-
-    volume {
-      name       = "logs"
-      mount_path = "/aci/logs"
-      read_only  = false
-      share_name = "${azurerm_storage_share.main.name}"
-
-      storage_account_name = "${azurerm_storage_account.main.name}"
-      storage_account_key  = "${azurerm_storage_account.main.primary_access_key}"
-    }
-  }
-
-  container {
-    name   = "sidecar"
-    image  = "microsoft/aci-tutorial-sidecar"
-    cpu    = "0.5"
-    memory = "1.5"
-  }
-
-  tags {
-    environment = "testing"
-  }
+resource "azurerm_subnet" "main" {
+  name                 = "${var.name}-subnet"
+  resource_group_name  = "${azurerm_resource_group.main.name}"
+  virtual_network_name = "${azurerm_virtual_network.main.name}"
+  address_prefix       = "10.0.1.0/24"
 }
